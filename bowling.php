8f53dec53c89a8a17e408089437fd37936d3ea71
mao <iceman20050712@gmail.com>
2017-09-12 00:35:11 +0800

    fulfilled all 3 test

--- bowling.php
+++ bowling.php
@@ -2,8 +2,24 @@
 class bowling {
 	public function countScore($scores) {
 		$total 	= 0;
-		foreach ($scores as $score) {
-			$total 	+= $score;
+		$round 	= 1;
+		$ball  	= 0;
+		foreach ($scores as $index => $score) {
+			if ($round > 10 ) {
+				break;
+			}
+
+			if ($score == 10) {
+				$total = $total + $score + $scores[$index + 1] + $scores[$index + 2];
+				$round++;
+			} else {
+				$total = $total + $score;
+				$ball++;
+				if ($ball == 2) {
+					$round++;
+					$ball = 0;
+				}
+			}
 		}
 		return $total;
 	}
