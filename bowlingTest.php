fe197d1e01da8dec3a2d16f91a6b989ff303862f
mao <iceman20050712@gmail.com>
2017-09-11 23:52:18 +0800

    add test 2, has error

--- bowlingTest.php
+++ bowlingTest.php
@@ -20,4 +20,24 @@ class bowlingTest extends PHPUnit_Framework_TestCase{
 		// assert
 		return 	$this->assertEquals($expect, $actual);
 	}
+
+	public function test_no_stike_no_spare() {
+		// arrange
+		$scores = array(1, 1,
+						1, 1, 
+						1, 1,
+						1, 1,
+						1, 1,
+						1, 1,
+						1, 1,
+						1, 1,
+						1, 1,
+						1, 1);
+		$expect	= 20;
+		// act
+		$bowling 	= new bowling();
+		$result		= $bowling->countScore($scores);
+		// assert
+		return 	$this->assertEquals($expect, $actual);
+	}
 }
