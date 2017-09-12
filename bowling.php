305e73106f8af33abb1feffa0b9c1f50aeff4c5d
mao <iceman20050712@gmail.com>
2017-09-13 00:30:26 +0800

    fulfilled all 6 test

--- bowling.php
+++ bowling.php
@@ -9,16 +9,28 @@ class bowling {
 			if ($round > 10) { 
 				break;
 			}
+
+			$next1 	= $scores[$index + 1];
+			$next2 	= $scores[$index + 2];
+
+			if (is_numeric($score) === false) {
+				throw new InvalidArgumentException();
+			}
+
 			$ball++;
 			if ($ball == 1 && $score == 10) { // strike
+				if (is_numeric($next1) === false || is_numeric($next2) === false) {
+					throw new InvalidArgumentException();
+				}
 				$total 	= $total + $score + $scores[$index + 1] + $scores[$index + 2];
 				$round++;
 				// clean
 				$ball = 0;
 			} elseif ($ball == 2 && ($roundScore + $score) == 10) { // spare
-
+				if (is_numeric($next1) === false) {
+					throw new InvalidArgumentException();
+				}
 				$total 	= $total + $roundScore + $score + $scores[$index + 1];
-
 				$round++;
 				// clean
 				$ball 	= 0;
